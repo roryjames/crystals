75f2d060228bd7471954c655182099cd07107320
Rory James <rjlanam@gmail.com>
2017-08-15 21:58:40 -0500

    some styles

---
+++ assets/javascript,/game.js
@@ -0,0 +1,8 @@
+var randomNum = Math.floor(Math.random() * 90 + 10);
+var ruby = Math.floor(Math.random() * 10);
+var sapphire = Math.floor(Math.random() * 10);
+var citrine = Math.floor(Math.random() * 10);
+var emerald = Math.floor(Math.random() * 10);
+
+console.log('hello');
+console.log(randomNum);
